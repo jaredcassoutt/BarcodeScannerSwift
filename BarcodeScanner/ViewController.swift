458798afed0b93ac0a720f8450e92b96d86d19dc
jared Cassoutt <jaredcass99@gmail.com>
2020-06-12 14:02:36 -0500

    Initial Commit

---
+++ BarcodeScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarcodeScanner
+//
+//  Created by Jared cassoutt on 6/12/20.
+//  Copyright © 2020 Jared Cassoutt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
